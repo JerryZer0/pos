8ba272f510a7c8836187ad2a0acaed6f4bdd557c
Jerry <jerry.zhang@oocl.com>
2018-07-08 13:30:07 +0800

    generateReceipt changed and all finished

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,8 +2,6 @@
 
 const { findKinds, findCount, findTable, getPromotion, calculateSubtotalAndAmount, generateReceipt} = require('../main/main')
 
-// describe('pso findKinds ', () => {
-
   const tags = [
     'ITEM000001',
     'ITEM000001',
@@ -22,16 +20,15 @@ const { findKinds, findCount, findTable, getPromotion, calculateSubtotalAndAmoun
 //     const buy_items_array = Array.from(buy_items);
 //     expect((JSON.stringify(buy_items_array)).toEqual(expectText));
 //   });
-// });
 
 describe('function findKinds() test', () => {
 
+  const expectText = '["ITEM000001","ITEM000003","ITEM000005"]';
   it('it should return the right result', () => {
 
     const buy_items = findKinds(tags);
-    //console.info(buy_items);
     const buy_items_array = Array.from(buy_items)
-    const expectText = '["ITEM000001","ITEM000003","ITEM000005"]';
+    
     expect(JSON.stringify(buy_items_array)).toEqual(expectText);
   });
 });
@@ -76,5 +73,21 @@ describe('function calculateSubtotalAndAmount() test', () => {
   it('it should return the right result', () => {
     const obj = calculateSubtotalAndAmount(receiptBase);
     expect(JSON.stringify(obj)).toEqual(expectText);
+  });
+});
+
+describe('function generateReceipt() test', () => {
+  const obj = {"itemList":[{"barcode":"ITEM000001","name":"雪碧","count":5,"unit":"瓶","price":3,"countAfterPromote":4,"subTotal":12},{"barcode":"ITEM000003","name":"荔枝","count":2.5,"unit":"斤","price":15,"countAfterPromote":2.5,"subTotal":37.5},{"barcode":"ITEM000005","name":"方便面","count":3,"unit":"袋","price":4.5,"countAfterPromote":2,"subTotal":9}],"reduce":7.5,"amount":58.5}; 
+  const expectText = `***<没钱赚商店>收据***
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+名称：荔枝，数量：2.5斤，单价：15.00(元)，小计：37.50(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：58.50(元)
+节省：7.50(元)
+**********************`;
+  it('it should return the right result', () => {
+    const receiptToString = generateReceipt(obj);
+    expect(receiptToString).toEqual(expectText);
   });
 });
